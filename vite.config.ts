3f20b3987eb675b644e83c05dc7a5b491ac8a551
Louistrash <patricknieborg@me.com>
2025-03-09 21:02:56 +0100

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dotenv from 'dotenv';
+import { loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      __SUPABASE_URL__: JSON.stringify(env.VITE_SUPABASE_URL),
+      __SUPABASE_ANON_KEY__: JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
+    },
+    plugins: [
+      remix({
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+        },
+      }),
+      tsconfigPaths(),
+    ],
+  };
 });
